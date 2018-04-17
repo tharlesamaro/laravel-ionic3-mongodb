bdd591b40c49b3b6219af653e2f67beb8628a2b9
tharlesamaro <tharlesamaro@gmail.com>
2018-04-17 02:58:59 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# laravel-ionic3-mongodb
---
+++ ionic3/src/pages/home/home.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { UsuarioProvider } from "../../providers/usuario/usuario";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  providers:[
+      UsuarioProvider,
+  ]
+})
+export class HomePage {
+
+  public usuarios = [];
+
+  public usuarioCadastro = {"nome":"", "idade":null};
+
+  constructor(public navCtrl: NavController, private usuarioService: UsuarioProvider) {
+    this.getUsuarios();
+  }
+
+  public getUsuarios() {
+    this.usuarioService.findAll().subscribe(response => this.usuarios = response);
+  }
+
+  public salvarUsuario() {
+      this.usuarioService.salvar(this.usuarioCadastro).subscribe(response => this.getUsuarios());
+  }
+
+}
